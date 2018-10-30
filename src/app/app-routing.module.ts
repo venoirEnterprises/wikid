96b1862bbbb5b25bcb0864d16fb27fe63755e6f7
vnorris <nvsibeeyt@gmail.com>
2018-10-30 16:59:46 +0000

    https://medium.com/@ryanchenkie_40935/angular-authentication-using-route-guards-bf7a4ca13ae3 beginning to add authentication

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,31 @@
-import {NgModule} from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
-import {HeroesComponent} from './heroes/heroes.component';
-import {DashboardComponent} from './dashboard/dashboard.component';
-import {HeroDetailComponent} from './hero-detail/hero-detail.component';
-
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HeroesComponent } from './heroes/heroes.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { AuthGuardService as AuthGuard } from './services/auth-guard.service';
 const routes: Routes = [
-  {path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-  {path: 'heroes', component: HeroesComponent},
-  {path: 'detail/:id', component: HeroDetailComponent},
-  {path: 'dashboard', component: DashboardComponent}
+	{
+		path: '',
+		redirectTo: '/heroes',
+		pathMatch: 'full'
+	},
+	{
+		path: 'heroes',
+		component: HeroesComponent
+	},
+	{
+		path: 'detail/:id',
+		component: HeroDetailComponent
+	},
+	{
+		path: 'dashboard',
+		component: DashboardComponent,
+		canActivate: [ AuthGuard ]
+	}
 ];
 @NgModule({
-  imports:
-    [RouterModule.forRoot(routes)],
-  exports:
-   [RouterModule]
+	imports: [ RouterModule.forRoot(routes) ],
+	exports: [ RouterModule ]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
-
-@Injectable({
-  providedIn: 'root'
-})
+import { JwtHelperService } from '@auth0/angular-jwt';
+@Injectable()
 export class AuthService {
-
-  constructor() { }
+	constructor(public jwtHelper: JwtHelperService) {}
+	// ...
+	public isAuthenticated(): boolean {
+		const token = localStorage.getItem('token');
+		// Check whether the token is expired and return
+		// true or false
+		return !this.jwtHelper.isTokenExpired(token);
+	}
 }
